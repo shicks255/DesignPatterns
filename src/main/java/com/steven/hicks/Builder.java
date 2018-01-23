7a71a2dd070fe16878eb0000724ea81b8013d770
shicks <shicks255@yahoo.com>
2018-01-22 21:24:52 -0500

    Builder pattern started

--- src/main/java/com/steven/hicks/Builder.java
+++ src/main/java/com/steven/hicks/Builder.java
@@ -4,8 +4,8 @@ public class Builder
 {
     public static void main(String[] args)
     {
+        Burger burger = new Burger.Builder().calories(250).servings(1).type("Turkey").size("Extra Large").build();
 
-
-
+        System.out.println(burger);
     }
 }
--- src/main/java/com/steven/hicks/Burger.java
+++ src/main/java/com/steven/hicks/Burger.java
@@ -4,18 +4,53 @@ public class Burger
 {
     private String size;
     private String type;
-    private String servings;
-    private String calories;
+    private int servings;
+    private int calories;
 
+    public String toString()
+    {
+        return "Burger Size " + size + " type " + type + " servings " + servings + " calories " + calories;
+    }
 
     public static class Builder
     {
-        private final int servings;
+        private int servings;
+        private int calories;
+        private String size;
+        private String type;
 
-        public Builder()
+        public Builder servings(int serving)
         {
-            servings = 1;
-
+            servings = serving;
+            return this;
         }
+        public Builder calories(int calories)
+        {
+            this.calories = calories;
+            return this;
+        }
+        public Builder size(String size)
+        {
+            this.size = size;
+            return this;
+        }
+        public Builder type(String type)
+        {
+            this.type = type;
+            return this;
+        }
+
+        public Burger build()
+        {
+            return new Burger(this);
+        }
+    }
+
+    private Burger(Builder builder)
+    {
+        calories = builder.calories;
+        size = builder.size;
+        type = builder.type;
+        servings = builder.servings;
     }
 }
